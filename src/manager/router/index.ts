33542e123994f462060b341de2c1c25f2a222bb5
DumiFlex <94956983+DumiFlex@users.noreply.github.com>
2026-04-26 21:17:49 +0300

    feat(spa): per-type routes + drop polymorphic modules.vue

--- src/manager/router/index.ts
+++ src/manager/router/index.ts
@@ -6,21 +6,21 @@ const routes: RouteRecordRaw[] = [
     path: "/",
     component: AppLayout,
     children: [
-      { path: "", redirect: "/modules" },
-      { path: "modules", name: "modules", component: () => import("../views/Modules.vue") },
-      { path: "modules/wildcard/new", name: "wildcard-new", component: () => import("../views/WildcardForm.vue") },
-      { path: "modules/wildcard/:id", name: "wildcard-edit", component: () => import("../views/WildcardForm.vue"), props: true },
-      { path: "modules/fixed-values/new", name: "fixed-values-new", component: () => import("../views/FixedValueForm.vue") },
-      { path: "modules/fixed-values/:id", name: "fixed-values-edit", component: () => import("../views/FixedValueForm.vue"), props: true },
+      { path: "", redirect: "/wildcards" },
+      { path: "dashboard", name: "dashboard", component: () => import("../views/Dashboard.vue") },
+      { path: "wildcards", name: "wildcards", component: () => import("../views/Wildcards.vue") },
+      { path: "wildcards/new", name: "wildcards-new", component: () => import("../views/WildcardForm.vue") },
+      { path: "wildcards/:id/edit", name: "wildcards-edit", component: () => import("../views/WildcardForm.vue"), props: true },
+      { path: "fixed-values", name: "fixed-values", component: () => import("../views/FixedValues.vue") },
+      { path: "fixed-values/new", name: "fixed-values-new", component: () => import("../views/FixedValueForm.vue") },
+      { path: "fixed-values/:id/edit", name: "fixed-values-edit", component: () => import("../views/FixedValueForm.vue"), props: true },
       { path: "categories", name: "categories", component: () => import("../views/Categories.vue") },
       { path: "import-export", name: "import-export", component: () => import("../views/ImportExport.vue") },
       { path: "test", name: "test", component: () => import("../views/TestRunner.vue") },
       { path: "settings", name: "settings", component: () => import("../views/Settings.vue") },
     ],
   },
+  { path: "/:pathMatch(.*)*", redirect: "/wildcards" },
 ];
 
-export default createRouter({
-  history: createWebHistory("/wp/"),
-  routes,
-});
+export default createRouter({ history: createWebHistory("/wp/"), routes });
